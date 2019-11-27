38f29d300c5b8b2a2c1110187f90537d9a979884
annazabn <annazabn@gmail.com>
2019-11-28 02:12:33 +0500

    Fix commands in entrypoint

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-if [ "$1" = 'build-index' ]; then
+if [ "$1" = 'build' ]; then
     python3 src/build.py
     python3 src/app.py
     exec echo 'build'
-elif [ "$1" = 'start-api' ]; then
+elif [ "$1" = 'start' ]; then
     python3 src/app.py
     exec echo 'api'
 else
